bf0875252543e2b9cc8d9b3a4a396a56157eea8d
sappo <kevinsapper88@gmail.com>
2014-02-07 14:59:07 +0100

    made methods static which are passed to c

--- ZeroSyncDesktop/zsconnector.cpp
+++ ZeroSyncDesktop/zsconnector.cpp
@@ -1,56 +1,49 @@
 #include "zsconnector.h"
-extern "C"
-{
-    #include "czmq.h"
-    #include "zyre.h"
-    #include "zyre_event.h"
-    #include "zs_fmetadata.h"
-    #include "zs_msg.h"
-    #include "zsync_peer.h"
-    #include "zsync_ftmanager.h"
-    #include "zsync_node.h"
-    #include "zsync_agent.h"
-}
 
-zlist_t* get_update(uint64_t from_state)
-{
-
-}
-
-void pass_update(char* sender, zlist_t* file_metadata)
-{
-
-}
-
-byte* get_chunk(char* path, uint64_t chunk_size, uint64_t offset)
-{
-
-}
-
-void pass_chunk(byte *chunk, char *path, uint64_t sequence, uint64_t offset)
-{
-
-}
-
-u_int64_t get_current_state()
-{
-    return 0;
-}
-
-ZSConnector::ZSConnector(QObject *parent) :
+ZSConnector::ZSConnector(QObject *parent, ZSDatabase *zsdatabase) :
     QObject(parent)
 {
+    database = zsdatabase;
+
     zsync_agent_t *zsAgent;
     zsAgent = zsync_agent_new();
-    zsync_agent_set_get_update(zsAgent, (void*) get_update);
-    zsync_agent_set_pass_update(zsAgent, (void*) pass_update);
+    zsync_agent_set_get_update(zsAgent, (void *) &ZSConnector::get_update);
+    zsync_agent_set_pass_update(zsAgent, (void *) &ZSConnector::pass_update);
 
-    zsync_agent_set_get_chunk(zsAgent, (void*) get_chunk);
-    zsync_agent_set_pass_chunk(zsAgent, (void*) pass_chunk);
+    zsync_agent_set_get_chunk(zsAgent, (void *) &ZSConnector::get_chunk);
+    zsync_agent_set_pass_chunk(zsAgent, (void *) &ZSConnector::pass_chunk);
 
-    zsync_agent_set_get_current_state(zsAgent, (void*) get_current_state);
+    zsync_agent_set_get_current_state(zsAgent, (void *) &ZSConnector::get_current_state);
 
     qDebug() << zsync_agent_start(zsAgent);
+}
+
+zlist_t* ZSConnector::get_update(uint64_t from_state)
+{
+    qDebug() << "get_update";
+    return NULL;
+}
+
+void ZSConnector::pass_update(char* sender, zlist_t* file_metadata)
+{
+    qDebug() << "pass_update";
+}
+
+byte* ZSConnector::get_chunk(char* path, uint64_t chunk_size, uint64_t offset)
+{
+    qDebug() << "get_chunk";
+    return NULL;
+}
+
+void ZSConnector::pass_chunk(byte *chunk, char *path, uint64_t sequence, uint64_t offset)
+{
+    qDebug() << "pass_chunk";
+}
+
+uint64_t ZSConnector::get_current_state()
+{
+    qDebug() << "current_state";
+    return database->getLatestState();
 }
 
 
--- ZeroSyncDesktop/zsconnector.h
+++ ZeroSyncDesktop/zsconnector.h
@@ -2,23 +2,32 @@
 #define ZSCONNECTOR_H
 
 #include <QObject>
+#include <QtDebug>
+#include <zsdatabase.h>
 #include <cstdint>
 #include <cstdlib>
 #include <cstdio>
-#include <QtDebug>
+#include <zmq.h>
+#include <czmq.h>
+#include <zyre.h>
+#include<zsync.h>
 
 class ZSConnector : public QObject
 {
 
 public:
-    explicit ZSConnector(QObject *parent = 0);
+    explicit ZSConnector(QObject *parent = 0, ZSDatabase *zsdatabase = 0);
+
+    void function(void);
 
 private:
-//    static zlist_t* get_update(u_int64_t from_state);
-//    static void pass_update(char* sender, zlist_t* file_metadata);
-//    static byte* get_chunk(char* path, u_int64_t chunk_size, u_int64_t offset);
-//    static void pass_chunk(byte* chunk, char* path, u_int64_t sequence, u_int64_t offset);
-//    static u_int64_t get_current_state();
+    static zlist_t* get_update(uint64_t from_state);
+    static void pass_update(char* sender, zlist_t* file_metadata);
+    static byte* get_chunk(char* path, uint64_t chunk_size, uint64_t offset);
+    static void pass_chunk(byte* chunk, char* path, uint64_t sequence, uint64_t offset);
+    static uint64_t get_current_state();
+
+    ZSDatabase *database;
 
 signals:
 
